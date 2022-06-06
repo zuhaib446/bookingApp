a4935b8c62a3d1cc6f98c06102ffe407d80d9c70
zuhaib446 <zuhaib.rustam@gmail.com>
2022-06-06 21:05:59 +0500

    setup mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HotelModule } from './hotel/hotel.module';
+import { config } from 'dotenv';
+import { MongooseModule } from '@nestjs/mongoose';
+config();
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGO_DB_URL), HotelModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
