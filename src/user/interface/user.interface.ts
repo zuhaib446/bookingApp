9ccfef2bdf5b2b69a0d8962c71626a84cee94e4c
zuhaib446 <zuhaib.rustam@gmail.com>
2022-06-10 03:00:54 +0500

    user

---
+++ src/user/interface/user.interface.ts
@@ -0,0 +1,13 @@
+export interface UserInterface {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    role: [string];
+    bankAccount: string;
+    phone: string;
+    address: string;
+    avatar: string;
+    isActive: boolean;
+    isVerified: boolean;
+}
