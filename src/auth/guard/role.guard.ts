9af6f9b3b95583efae8cf02c4a3729bf9074c5c3
zuhaib446 <zuhaib.rustam@gmail.com>
2022-06-14 03:21:43 +0500

    role and auth gard decorator

---
+++ src/auth/guard/role.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable, ExecutionContext, ForbiddenException, UnauthorizedException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class RoleGuard extends AuthGuard('jwt') {
+    constructor(private readonly reflector: Reflector) {
+        super();
+    }
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const roles = this.reflector.get<string[]>('roles', context.getHandler());
+        if (!roles) {
+            return true;
+        }
+        const request = context.switchToHttp().getRequest();
+        const user = request.user;
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+        const hasRole = () => user.roles.some(role => roles.includes(role));
+        if (user && user.roles && hasRole()) {
+            return true;
+        }
+        throw new ForbiddenException();
+    }
+}
