77374643e3bb7d513722bf51f63b38c1d3141e3f
zuhaib446 <zuhaib.rustam@gmail.com>
2022-06-19 02:36:01 +0500

    validator  dto for users

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,12 +1,28 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class UserDto {
+    @IsString()
     firstName: string;
+
+    @IsString()
     lastName: string;
+
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
     password: string;
+
+    @IsString()
     role: string;
 }
 
 export class UserLoginDto {
+    @IsEmail()
     email: string;
+    @IsString()
     password: string;
 }
