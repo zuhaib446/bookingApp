269fcdcfbdf013bdc9a619490eebc1817710c4df
zuhaib446 <zuhaib.rustam@gmail.com>
2022-06-19 02:35:49 +0500

    validator  dto for rooms

--- src/rooms/dto/rooms.dto.ts
+++ src/rooms/dto/rooms.dto.ts
@@ -1,9 +1,23 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class RoomsDto {
+    @IsString()
+    @IsNotEmpty()
     hotelId: string;
+
+    @IsString()
     roomType: string;
+
+    @IsString()
     roomDescription: string;
+
+    @IsNumber()
+    @IsNotEmpty()
     roomPrice: number;
+
+    @IsString()
     roomImages: [string];
+
+    @IsNumber()
     maxPeople: number;
 }
