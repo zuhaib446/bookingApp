d58feb6983b3aa34b62848f91c70d083ac4fe826
zuhaib446 <zuhaib.rustam@gmail.com>
2022-06-21 02:29:26 +0500

    new booking

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { BookingDto } from './dto/booking.dto';
 import { BookingService } from './booking.service';
 import { BookingInterface } from './interface/booking.interface';
@@ -13,16 +13,18 @@ export class BookingController {
     @Role('user')
     @Post()
     async create(
+        @Req() req: any,
         @Body() bookingDto: BookingDto
     ): Promise<BookingInterface> {
-        console.log(bookingDto);
-        return await this.bookingService.create(bookingDto)
+        const userID = req.user.id
+        return await this.bookingService.create(userID, bookingDto)
     }
 
     @UseGuards(AuthGuard('jwt'))
     @Role('admin', 'user', 'owner')
     @Get()
-    async findAll(): Promise<BookingInterface[]> {
+    async findAll(
+    ): Promise<BookingInterface[]> {
         return await this.bookingService.findAll()
     }
 
