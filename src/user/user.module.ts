5c79e14783b3cc21acd878e7a6d9b20cfe71b6cd
zuhaib446 <zuhaib.rustam@gmail.com>
2022-06-21 15:39:18 +0500

    multer upload user avatar

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,21 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserSchema } from './schema/user.schema';
 import { AuthModule } from 'src/auth/auth.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { existsSync, mkdirSync } from 'fs';
+
+// create a folder called uploads in the root directory
+if (!existsSync('./uploads')) {
+  mkdirSync('./uploads');
+}
 
 @Module({
   imports: [
+    MulterModule.register({
+      dest: './uploads',
+      preservePath: true,
+      limits: { fileSize: 10 * 1024 * 1024 },
+    }),
     MongooseModule.forFeature([
       { name: 'User', schema: UserSchema },
     ]),
